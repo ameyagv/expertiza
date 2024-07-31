4e080414ca2727adba626347cdd5630f145c1a0e
dinesh-pasupuleti <78731527+dinesh-pasupuleti@users.noreply.github.com>
2024-07-31 15:37:29 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -43,21 +43,23 @@ role :db,  %w[agvaicha@152.7.176.86]
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
+set :ssh_options, {
+  keys: %w(~/.ssh/id_rsa),
+  forward_agent: true,
+  auth_methods: %w(publickey),
+  user: 'agvaicha',
+  verify_host_key: :secure
+}
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+server '152.7.176.86',
+  user: 'agvaicha',
+  roles: %w{web app},
+  ssh_options: {
+    keys: %w(~/.ssh/id_rsa),
+    forward_agent: true,
+    auth_methods: %w(publickey),
+    verify_host_key: :secure,
+    config: false,
+    term: 'dumb'
+  }
